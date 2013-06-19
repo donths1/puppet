041c5ab10211f20a7d61633f0539a0d327006377
Sreedhar <sreedhar.donthi@emc.com>
2013-06-19 16:25:34 +0530

    rabbitmq

---
+++ modules/rabbit/manifests/init.pp
@@ -0,0 +1,41 @@
+class rabbit { 
+   $vm_home = hiera('vm_home')
+   $rb_home = hiera('rb_home')
+   $rb_sbin = hiera('rb_sbin')
+   $rb_tar = hiera('rb_tar')
+   $otp_tar = hiera('otp_tar')
+   $otp_home = hiera('otp_home')
+
+   # exec { 'erluz':
+   #   command => '/bin/tar -xvf ${otp_tar} -C /opt/vmware/',
+    # before => Exec['erlconfig'],
+    # require => File["${vm_home}/otp_src_R16B.tar.gz"],
+   # }
+   # exec { 'erlconfig' :
+   #    command => '${otp_home}/configure && make && sudo make install',
+   #    before => Exec['rabbituz']
+   # }
+    exec { 'rabbituz':
+     command => "/bin/tar -xvf ${rb_tar} -C /opt/vmware/",
+     before => Exec['enablemanagement'],
+     require => File["${vm_home}/vfabric-rabbitmq-server-3.0.4.tar.gz"],
+    }
+  # file {"${vm_home}/otp_src_R16B.tar.gz":
+  #    ensure => file,
+  #    owner => root,
+  #    mode => '0744',
+ #  }
+   file {"${vm_home}/vfabric-rabbitmq-server-3.0.4.tar.gz":
+      ensure => file,
+      owner => root,
+      mode => '0744',
+   }
+   exec {'enablemanagement':
+      command => "${rb_sbin}/rabbitmq-plugins enable rabbitmq_management",
+   }
+   service { 'rabsever' :
+       ensure => running,
+       start => "${rb_sbin}/rabbitmq-server -detached",
+       require => Exec['enablemanagement'],
+   }
+}
