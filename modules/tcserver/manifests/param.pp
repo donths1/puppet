3cb7151e19b542a75759f060766a43b93051752d
Sreedhar <sreedhar.donthi@emc.com>
2013-06-19 16:53:32 +0530

    ch

--- modules/tcserver/manifests/param.pp
+++
@@ -1,6 +0,0 @@
-class param {
-   $vm_home = '/opt/vmware'
-   $tc_zipfile = "${vm_home}/vfabric-tc-server-standard-2.9.2.RELEASE.zip"
-   $tc_home = "${vm_home}/vfabric-tc-server-standard-2.9.2.RELEASE"
-   $tc_bin = "${tc_home}/myinstance/bin"
-}
